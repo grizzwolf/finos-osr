e7fd2591eacb9dc83843b1c0e55f9cbb294d5156
Aaron Griswold <aaron@grizzwolf.com>
2019-07-01 14:42:31 -0400

    changed osr URL links (from deg)

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -62,7 +62,7 @@ const siteConfig = {
 
   // You may provide arbitrary config keys to be used as needed by your
   // template. For example, if you need your repo's URL...
-  repoUrl: 'https://github.com/grizzwolf/finos-deg'
+  repoUrl: 'https://github.com/grizzwolf/finos-osr'
 };
 
 module.exports = siteConfig;
